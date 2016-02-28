9851436038b2016cf213a37862f61126fb156c87
Eduardo Cuducos <cuducos@gmail.com>
2016-02-28 12:14:50 -0300

    Move create_nickname to Profile model

--- valentina/app/models.py
+++ valentina/app/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from faker import Factory
 
 
 class Profile(models.Model):
@@ -15,9 +16,20 @@ class Profile(models.Model):
     gender = models.CharField('gênero', max_length=1, choices=GENDERS)
     timezone = models.CharField('fuso-horário', max_length=255)
     nickname = models.CharField('apelido', max_length=255)
+    first_access = models.BooleanField('primeiro acesso', default=True)
 
     def __str__(self):
         return self.nickname if self.nickname else self.user.get_full_name()
+
+    @staticmethod
+    def create_nickname():
+        faker = Factory.create(settings.LANGUAGE_CODE)
+        suffix = 'a'
+        exceptions = ('Valentina', 'Lucca')
+        nickname = faker.first_name_female()
+        while not nickname.endswith(suffix) or nickname in exceptions:
+            nickname = faker.first_name_female()
+        return nickname
 
     class Meta:
         ordering = ['nickname']
