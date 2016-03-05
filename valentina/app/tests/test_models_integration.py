c1f0b217283e295497cd6c0b5b0e40474a628ddc
Eduardo Cuducos <cuducos@gmail.com>
2016-03-05 16:25:23 -0300

    Split fixtures

--- valentina/app/tests/test_models_integration.py
+++ valentina/app/tests/test_models_integration.py
@@ -4,10 +4,12 @@ from valentina.app.models import Profile, Chat, Affiliation, Message
 
 class TestModelsIntegration(TestCase):
 
-    fixtures = ['models.json']
+    fixtures = ['users.json', 'profiles.json', 'chats.json',
+                'affiliations.json', 'messages.json']
 
     def test_objects_were_created(self):
-        self.assertEqual(2, Profile.objects.count())
-        self.assertEqual(1, Chat.objects.count())
-        self.assertEqual(2, Affiliation.objects.count())
-        self.assertEqual(2, Message.objects.count())
+        with self.subTest():
+            self.assertEqual(2, Profile.objects.count())
+            self.assertEqual(1, Chat.objects.count())
+            self.assertEqual(2, Affiliation.objects.count())
+            self.assertEqual(2, Message.objects.count())
