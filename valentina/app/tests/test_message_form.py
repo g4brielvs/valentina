e13fcf64a808b19b8e4832356cbf98a32539cbc3
Eduardo Cuducos <cuducos@gmail.com>
2016-03-06 22:25:39 -0300

    Clean up

--- valentina/app/tests/test_message_form.py
+++ valentina/app/tests/test_message_form.py
@@ -3,9 +3,8 @@ from django.test import TestCase
 from valentina.app.forms import MessageForm
 
 
-class MessageFormtest(TestCase):
+class MessageFormTest(TestCase):
 
     def test_form_has_fields(self):
-        """Form must have 4 fields"""
         form = MessageForm()
         self.assertSequenceEqual(list(form.fields), ['content', 'chat'])
