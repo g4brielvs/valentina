86d6cefd3da3f5a5f2ba6df52922af3f93ac4f76
Eduardo Cuducos <cuducos@gmail.com>
2016-03-19 15:48:59 -0300

    Bug fix: use get_or_create to allow new users

--- valentina/app/oauth.py
+++ valentina/app/oauth.py
@@ -5,9 +5,7 @@ FB_GENDER = {'feminino': Profile.FEMALE, 'masculino': Profile.MALE}
 
 def save_profile(backend, user, response, *args, **kwargs):
     if backend.name == 'facebook':
-        profile = Profile.objects.get(user=user)
-        if profile is None:
-            profile = Profile(user=user)
+        profile, created = Profile.objects.get_or_create(user=user)
         profile.gender = FB_GENDER.get(response.get('gender'), Profile.OTHER)
         profile.timezone = response.get('timezone', '')
         profile.access_token = response.get('access_token', '')
